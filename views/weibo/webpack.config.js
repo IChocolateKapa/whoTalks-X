3e41b5d68a40c3316b5ea00ef7a46e0e5d337d3b
IChocolateKapa <echochocolatekapa@163.com>
2016-04-15 13:26:06 +0800

    整天看我提交的东西无不无聊啊？

---
+++ views/weibo/webpack.config.js
@@ -0,0 +1,32 @@
+//var path = require('path');
+//var webpack = require('webpack');
+
+module.exports = {
+    entry: "./src/app.js",
+    output: {
+        path: 'dist',
+        filename: 'bundle.js'
+    },
+    /*resolveLoader: {
+        root: path.join(__dirname, 'node_modules')
+    },*/
+    module: {
+        loaders: [
+            {
+                test: /\.vue$/,
+                loader: 'vue'
+            },
+            {
+                test: /\.css$/,
+                loader: "style!css"
+            },//需要用双引号才不会报错哟~
+            //{ test: /\.png$/, loader: "url-loader?mimetype=image/png"},
+        ]
+    },
+    babel: {
+        presets: ['es2015'],
+        plugins: ['transform-runtime']
+    },
+    devtool: 'source-map',
+    watch: true
+};
