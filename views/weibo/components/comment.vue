20f87cd868e776606fadcea852681e070e06bf87
IChocolateKapa <echochocolatekapa@163.com>
2016-04-27 15:23:53 +0800

    fix v-if v-else bug

--- views/weibo/components/comment.vue
+++ views/weibo/components/comment.vue
@@ -1,25 +1,31 @@
 <template>
     <div class="commentPanel">
-        <div v-if="cmtdata" class="commentItem" v-for="cmt in cmtdata">
-            <img class="headimg" src='{{cmt.headimg}}'  alt="头像"/>
-            <div class="dongtan-content">
-                <p class="author">{{cmt.author}}</p>
-                <p class="tip"><span>{{cmt.time}}</span></p>
-                <p class="content">
-                    {{cmt.content}}
-                </p>
-            </div>
-            <div class="like" :class="{ 'active': cmt.likes == 0? true: false}" >
-                <span>{{cmt.likes}}</span>
+        <div v-show="cmtdata.length > 0 ? true : false" class="commentItem">
+            <div v-for="cmt in cmtdata">
+                <img class="headimg" src='{{cmt.headimg}}'  alt="头像"/>
+                <div class="dongtan-content">
+                    <p class="author">{{cmt.author}}</p>
+                    <p class="tip"><span>{{cmt.time}}</span></p>
+                    <p class="content">
+                        {{cmt.content}}
+                    </p>
+                </div>
+                <div class="like" :class="{ 'active': cmt.likes == 0? true: false}" >
+                    <span>{{cmt.likes}}</span>
+                </div>
             </div>
         </div>
 
-        <p v-else class="none">暂无评论</p>
+        <div v-else class="none">暂无评论</div>
+
     </div>
 </template>
 
 <script>
     module.exports = {
         props: ['cmtdata']
-    }
+    };
+    /*export default {
+        props: ['cmtdata']
+    }*/
 </script>
